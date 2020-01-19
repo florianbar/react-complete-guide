8cb45725363a808259194eade138c919e1496629
florianbar <florianbar01@gmail.com>
2020-01-19 11:50:40 +0200

    Added a click handler to the Person component

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
     otherState: "Some other value"
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log("Was clicked");
     this.setState({ 
       persons: [
-        { name: 'Florian', age: 33 },
+        { name: newName, age: 33 },
         { name: 'Candice', age: 32 },
         { name: 'Abby', age: 5 }
       ]
@@ -28,10 +28,19 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={ this.switchNameHandler }>Switch Name</button>
-        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
-        <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }>I like Korean dramas!</Person>
-        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
+        <button onClick={this.switchNameHandler.bind(this, "Florian")}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          myClick={this.switchNameHandler.bind(this, "Flo!")}>
+            I like Korean dramas!
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
