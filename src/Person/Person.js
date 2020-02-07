45128a24423e5d4bef444866ef7096b257f5fd72
Flo <Florianb@payspace.com>
2020-02-07 06:45:47 +0200

    Added media queries with CSS modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
-//import './Person.css';
-
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    padding: 16px;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-`;
+import classes from './Person.css';
 
 const person = (props) => {
     return (
-        <StyledDiv>
+        <div className={classes.Person}>
             <p onClick={props.myClick}>I'm {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changeName} value={props.name} />
-        </StyledDiv>
+        </div>
     );
 };
 
