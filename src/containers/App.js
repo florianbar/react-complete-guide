92eddf1541714278ab6e573a6fc037e4a5273a90
Flo <Florianb@payspace.com>
2020-03-02 07:14:23 +0200

    Added check to update state everytime a change occurs

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,8 @@ class App extends Component {
     ],
     otherState: "Some other value",
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -40,8 +41,11 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ 
-      persons: persons
+    this.setState((prevState, props) => { 
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      };
     });
   }
 
